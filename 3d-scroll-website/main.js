8ed80b199a3f38537bae129aa29f328d7e6104ad
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-20 23:02:46 +0300

    Files

--- 3d-scroll-website/main.js
+++ 3d-scroll-website/main.js
@@ -6,26 +6,32 @@ const scene = new THREE.Scene();
 // create sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64); //height, segments
 const material = new THREE.MeshBasicMaterial({
-  color: '#00ff83'
+  color: '#00df83'
 })
 
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
+// sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+
 
 // light
-const light = new THREE.PointLight(0xffffff, 1, 100); //
-light.position.set(0, 10, 10);
+const light = new THREE.PointLight(0xffffff, 10, 100); // light.position.set(0, 10, 10); x,y,z position
 scene.add(light);
 
 //Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600,0.1, 100) // field of view, aspect ratio, clipping points
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height) // field of view, aspect ratio, clipping points
 camera.position.z = 20;
 scene.add(camera);
 
 // renderer
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 
