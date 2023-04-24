2537a7441f1d9ab9eaacd4cc0a5c8243cdddc6be
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-24 22:07:01 +0300

    App

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,20 @@
-const App  = () => {
-   return (
-    <>
-    3d Developer    
-    </>
+import { BrowserRouter } from 'react-router-dom';
+
+import {
+  About, Contact, Experience, Feedbacks,
+  Hero, Navbar, Tech, Works, StarsCanvas
+} from './components/'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
