17f41b80a6dcf3cab3e342c7452fb34fe5cf2c29
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-25 23:31:00 +0300

    Creating Three Js using Mesh using react fiber Working with file from public directory ./desktop_pc/scene.gltf

--- Portfolio/src/components/canvas/Computers.jsx
+++ Portfolio/src/components/canvas/Computers.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import {Suspense, useEffect, useState} from 'react';
+import {Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity ={0.15}
+      groundColor = "black" />
+    </mesh>
   )
 }
 
